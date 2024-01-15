904c1d24be52774b4abb575d32a3558ca8e2d15c
Romel180149 <mosiurrahmanromel180149@gmail.com>
2024-01-15 21:32:54 +0600

    .env.local file done

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -4,13 +4,21 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCwN3yMxshWWFx6PIYnuH1Av9z0NzFWKKQ",
+//   authDomain: "real-state-platform-fe7da.firebaseapp.com",
+//   projectId: "real-state-platform-fe7da",
+//   storageBucket: "real-state-platform-fe7da.appspot.com",
+//   messagingSenderId: "588826664923",
+//   appId: "1:588826664923:web:b29f96964331064a95d1fe"
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyCwN3yMxshWWFx6PIYnuH1Av9z0NzFWKKQ",
-  authDomain: "real-state-platform-fe7da.firebaseapp.com",
-  projectId: "real-state-platform-fe7da",
-  storageBucket: "real-state-platform-fe7da.appspot.com",
-  messagingSenderId: "588826664923",
-  appId: "1:588826664923:web:b29f96964331064a95d1fe"
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
